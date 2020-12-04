67c946b35b3cfb62581b3133cf6295fbf222c895
H. Kamran <hkamran@unisontech.org>
2020-12-03 23:28:10 -0800

    Fix .gitignore, update main.js to remove VueSVGIcon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import VueSVGIcon from "vue-svgicon";
 
-Vue.use(VueSVGIcon);
 Vue.config.productionTip = false;
 
 new Vue({
