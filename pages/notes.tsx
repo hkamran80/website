2339287312b27500d62ed0037896d3f8dc020e6d
H. Kamran <hkamran@unisontech.org>
2022-08-01 15:46:17 -0700

    Convert the notes page to static generation

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -3,48 +3,52 @@ import Head from "next/head";
 import Layout from "../components/Layout";
 import Link from "next/link";
 import NoteCard from "../components/NoteCard";
-import { StateContext } from "./_app";
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
+import { WRITINGS_URL } from "../data/constants";
+import { sortByPublishDate } from "../lib/writings";
+import { Writing } from "../types/writings";
 
-const Notes: NextPage = () => {
+type Props = { notes: Writing[] };
+
+const Notes: NextPage<Props> = ({ notes }) => {
     return (
-        <StateContext.Consumer>
-            {(state) => (
-                <Layout>
-                    <Head>
-                        <title>Notes | H. Kamran</title>
-                    </Head>
+        <Layout>
+            <Head>
+                <title>Notes | H. Kamran</title>
+            </Head>
 
-                    <h1 className="text-3xl font-semibold flex items-center">
-                        <span className="flex-1">Notes</span>
-                        <a
-                            href="/feed/atom"
-                            target="_blank"
-                            rel="nofollow noreferrer"
-                            title="Atom feed"
-                            className="text-gray-400"
-                            dangerouslySetInnerHTML={{
-                                __html: feather.icons.rss.toSvg(),
-                            }}
-                        />
-                    </h1>
-                    <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
-                        {state.notes.map((note, index) => (
-                            <Link
-                                key={index}
-                                href={`/note/${note.id}`}
-                                passHref
-                            >
-                                <a>
-                                    <NoteCard note={note} />
-                                </a>
-                            </Link>
-                        ))}
-                    </div>
-                </Layout>
-            )}
-        </StateContext.Consumer>
+            <h1 className="text-3xl font-semibold flex items-center">
+                <span className="flex-1">Notes</span>
+                <a
+                    href="/feed/atom"
+                    target="_blank"
+                    rel="nofollow noreferrer"
+                    title="Atom feed"
+                    className="text-gray-400"
+                    dangerouslySetInnerHTML={{
+                        __html: feather.icons.rss.toSvg(),
+                    }}
+                />
+            </h1>
+            <div className="mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
+                {notes.map((note, index) => (
+                    <Link key={index} href={`/note/${note.id}`} passHref>
+                        <a>
+                            <NoteCard note={note} />
+                        </a>
+                    </Link>
+                ))}
+            </div>
+        </Layout>
     );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+    const res = await fetch(WRITINGS_URL);
+    const writings = await res.json();
+    const notes = (writings.notes as Writing[]).sort(sortByPublishDate);
+
+    return { props: { notes } };
 };
 
 export default Notes;
