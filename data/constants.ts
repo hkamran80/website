c11674febbfda3b0ebe446301f9e899a4920b035
H. Kamran <hkamran@unisontech.org>
2022-08-03 21:32:14 -0700

    Switch to the `caption-testing` branch in `hkamran80/articles`

--- data/constants.ts
+++ data/constants.ts
@@ -1,6 +1,6 @@
 export const WRITINGS_URL =
-    "https://raw.githubusercontent.com/hkamran80/articles/main/markdown/contents.json";
+    "https://raw.githubusercontent.com/hkamran80/articles/caption-testing/markdown/contents.json";
 export const BASE_WRITINGS_URL =
-    "https://raw.githubusercontent.com/hkamran80/articles/main/markdown";
+    "https://raw.githubusercontent.com/hkamran80/articles/caption-testing/markdown";
 
 export const SHOWCASE_URL = "https://raw.hkamran.com/hkamran/creations";
