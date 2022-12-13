ba251b698847c11b9764f962c2b72f2a8c2e5ab0
H. Kamran <hkamran@hkamran.com>
2022-12-12 22:26:57 -0800

    Add photos link

--- data/navigation.ts
+++ data/navigation.ts
@@ -24,4 +24,5 @@ export const links = [
     { href: "/articles", name: "Articles", aliases: ["/article", "/tag"] },
     { href: "/notes", name: "Notes", aliases: ["/note", "/tag"] },
     { href: "/programs", name: "Programs", aliases: ["/program"] },
+    { href: "https://unsplash.com/@hkamran", name: "Photos", aliases: [] },
 ];
