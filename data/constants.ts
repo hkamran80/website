b10240c5be7af5bab5530e8cf759feb4563928b8
H. Kamran <hkamran@hkamran.com>
2023-01-14 01:04:01 -0800

    Remove unnecessary constants

--- data/constants.ts
+++ data/constants.ts
@@ -19,27 +19,4 @@ export const BASE_WRITINGS_URL =
     "https://raw.githubusercontent.com/hkamran80/articles/main/markdown";
 
 export const SHOWCASE_URL = "https://raw.hkamran.com/hkamran/creations";
-
-export const UNSPLASH_PHOTOS_URL =
-    "https://api.unsplash.com/users/hkamran/photos";
-
 export const UNSPLASH_URL = "https://unsplash.com/@hkamran";
-
-export const KNOWN_LANGUAGES = [
-    {
-        name: "Python",
-        icon: siPython,
-    },
-    { name: "JavaScript", icon: siJavascript },
-    { name: "TypeScript", icon: siTypescript },
-    { name: "Vue.js", icon: siVuedotjs },
-    { name: "React", icon: siReact },
-    { name: "Next.js", icon: siNextdotjs },
-    { name: "Java", icon: siJava },
-    { name: "Kotlin", icon: siKotlin },
-    { name: "Flutter", icon: siFlutter },
-    { name: "Dart", icon: siDart },
-    { name: "Swift", icon: siSwift },
-    { name: "SwiftUI", icon: siSwift },
-    { name: "Go", icon: siGo },
-];
