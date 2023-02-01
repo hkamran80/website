34089ecf580be46fee7c81d47809bd87638ca3bb
H. Kamran <hkamran@hkamran.com>
2023-01-31 21:33:31 -0800

    Update fallback

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -165,7 +165,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
         params: { slug: article.id },
     }));
 
-    return { paths, fallback: false };
+    return { paths, fallback: "blocking" };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
--- pages/note/[slug].tsx
+++ pages/note/[slug].tsx
@@ -121,7 +121,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
         params: { slug: note.id },
     }));
 
-    return { paths, fallback: false };
+    return { paths, fallback: "blocking" };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
