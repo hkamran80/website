c15eeb362845a3156b1ed1b08262bd8faa77356b
H. Kamran <hkamran@hkamran.com>
2023-03-08 10:54:22 -0800

    Switch to new path

--- data/constants.ts
+++ data/constants.ts
@@ -3,6 +3,6 @@ export const WRITINGS_URL =
 export const BASE_WRITINGS_URL =
     "https://raw.githubusercontent.com/hkamran80/articles/main/markdown";
 
-export const SHOWCASE_URL = "https://assets.hkamran.com/creations";
+export const SHOWCASE_URL = "https://assets.hkamran.com/showcase";
 export const UNSPLASH_URL =
     "https://unsplash.com/@hkamran?utm_source=hkwebsite&utm_medium=referral";
