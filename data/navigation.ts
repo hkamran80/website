db2cd442bc1850f1890720d8ddaf7eb713c58468
H. Kamran <hkamran@hkamran.com>
2023-05-02 10:53:29 -0700

    Extract social links into component, add `rels` attribute

--- data/navigation.ts
+++ data/navigation.ts
@@ -7,9 +7,21 @@ import {
     siMastodon,
 } from "simple-icons/icons";
 import { UNSPLASH_URL } from "./constants";
+import { SimpleIcon } from "simple-icons";
 
-export const socialIcons = [
-    { url: "https://github.com/hkamran80", icon: siGithub, color: null },
+type Social = {
+    url: string;
+    icon: SimpleIcon;
+    color: string | null;
+    rels?: string[];
+};
+export const socialIcons: Social[] = [
+    {
+        url: "https://github.com/hkamran80",
+        icon: siGithub,
+        color: null,
+        rels: ["authn"],
+    },
     { url: "https://twitter.com/hkamran80", icon: siTwitter, color: "#1DA1F2" },
     { url: "https://vmst.io/@hkamran", icon: siMastodon, color: null },
     {
