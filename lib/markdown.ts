a3c900b578fdd8a2f996e33574f0c0368db72b16
H. Kamran <hkamran@hkamran.com>
2023-06-11 14:32:46 -0700

    Add `ref=` parameter to external Markdown links

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,10 +1,14 @@
 import MarkdownIt from "markdown-it";
 import markdownItLinkAttributes from "markdown-it-link-attributes";
+import markdownItReplaceLink from "markdown-it-replace-link";
 import markdownItPrism from "markdown-it-prism";
 import markdownItImageFigures from "markdown-it-image-figures";
 import markdownItFootnote from "markdown-it-footnote";
 import "prismjs/plugins/toolbar/prism-toolbar";
 import "prismjs/plugins/autoloader/prism-autoloader";
+
+const checkIfLocalLink = (link: string) =>
+    link.startsWith("/") || link.startsWith("https://hkamran.com");
 
 export const renderMarkdown = (
     content: string,
@@ -12,13 +16,26 @@ export const renderMarkdown = (
     images: boolean = false,
     footnotes: boolean = false,
 ): string => {
-    let md = new MarkdownIt({}).use(markdownItLinkAttributes, {
-        matcher: (href: string) => !href.startsWith("https://hkamran.com"),
-        attrs: {
-            target: "_blank",
-            rel: "noopener noreferrer",
-        },
-    });
+    let md = new MarkdownIt({})
+        .use(markdownItLinkAttributes, {
+            matcher: (href: string) => !checkIfLocalLink(href),
+            attrs: {
+                target: "_blank",
+                rel: "noopener noreferrer",
+            },
+        })
+        .use(markdownItReplaceLink, {
+            processHtml: true,
+            replaceLink: (link: string) => {
+                if (checkIfLocalLink(link)) {
+                    return link;
+                } else {
+                    return `${link}${
+                        link.includes("?") ? "&" : "?"
+                    }ref=hkamran.com`;
+                }
+            },
+        });
 
     if (code) {
         md = md.use(markdownItPrism, { plugins: ["toolbar", "autoloader"] });
--- types/declarations.d.ts
+++ types/declarations.d.ts
@@ -1,2 +1,3 @@
 // TODO: Create fully typed version of this library
 declare module "markdown-it-image-figures";
+declare module "markdown-it-replace-link";
