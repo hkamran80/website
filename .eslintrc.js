b15f2ffe56c3e4ea5fd908c6a66556d8b8db8a8b
H. Kamran <hkamran@hkamran.com>
2024-04-04 15:24:53 -0700

    Allow comma dangling

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
     extends: ["next/core-web-vitals", "@hkamran/eslint-config"],
     rules: {
         "react-hooks/exhaustive-deps": "off",
+        "comma-dangle": "always",
     },
 };
