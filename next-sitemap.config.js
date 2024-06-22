64171255683618a641b96112b1cc9eec3d4c440d
H. Kamran <hkamran@hkamran.com>
2024-06-21 23:31:11 -0700

    Add a larger list of blocked crawlers

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,70 @@
 /** @type {import('next-sitemap').IConfig} */
 
+// Source: https://www.foundationwebdev.com/2023/11/which-web-crawlers-are-associated-with-ai-crawlers
+const llmUserAgents = [
+    // Anthrophic
+    "anthropic-ai",
+    "Claude-Web",
+
+    // Apple
+    "Applebot-Extended",
+
+    // ByteDance
+    "Bytespider",
+
+    // Common Crawl
+    "CCBot",
+
+    // OpenAI
+    "ChatGPT-User",
+    "GPTBot",
+
+    // Cohere AI
+    "cohere-ai",
+
+    // Diffbot
+    "Diffbot",
+
+    // Meta
+    "FacebookBot",
+
+    // Google
+    "GoogleOther",
+    "Google-Extended",
+
+    // The Hive
+    "ImagesiftBot",
+
+    // Webz.io
+    "OmigiliBot",
+    "Omigili",
+];
+
+// Source: https://darkvisitors.com/agents
+const otherUserAgents = [
+    // Adbeat
+    "adbeat_bot",
+
+    // Google
+    "Adsbot-Google",
+    "Adsbot-Google-Mobile",
+
+    // aiHit
+    "aiHitBot",
+
+    // Anders Pink
+    "AndersPinkBot",
+
+    // Awario
+    "AwarioBot",
+    "AwarioSmartBot",
+
+    // Bitsight
+    "BitSightBot",
+];
+
+const fullyBlockedUserAgents = [...llmUserAgents, ...otherUserAgents];
+
 module.exports = {
     siteUrl: "https://hkamran.com",
     changefreq: "daily",
@@ -14,10 +79,20 @@ module.exports = {
                 allow: "/",
                 disallow: ["/_next/", "/api/", "/*.png"],
             },
-            {
-                userAgent: "GPTBot",
+
+            ...fullyBlockedUserAgents.map((userAgent) => ({
+                userAgent,
                 disallow: "/",
-            },
+            })),
+
+            //{
+            //    userAgent: "GPTBot",
+            //    disallow: "/",
+            //},
+            //{
+            //    userAgent: "Applebot-Extended",
+            //    disallow: "/",
+            //},
         ],
     },
 };
