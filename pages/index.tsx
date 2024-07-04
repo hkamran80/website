9e4c12639b129f6292cdb7ed7140c9db9ebd891a
H. Kamran <hkamran@hkamran.com>
2024-07-03 18:32:54 -0700

    Remove "Sponsor" button

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,13 +39,6 @@ const Home: NextPage<Props> = ({ latestArticle, featuredShowcase }) => (
                     footerClasses="pb-12 text-gray-500 justify-center flex flex-col space-y-4 justify-center"
                     footer={
                         <>
-                            <NavLink
-                                href="/sponsor"
-                                className="text-center uppercase tracking-widest transition duration-300 hover:scale-125 hover:text-pink-700"
-                            >
-                                Sponsor
-                            </NavLink>
-
                             <div className="flex flex-row justify-center space-x-7">
                                 <SocialLinks classNames="h-6 w-6 fill-gray-500 transition duration-300 hover:scale-125 hover:fill-pink-700" />
                             </div>
