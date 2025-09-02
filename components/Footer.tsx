3d064aeaa338c629f3104452045324128848acc9
H. Kamran <hkamran@hkamran.com>
2025-09-02 16:02:13 -0700

    Add no-AI notice

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,30 @@
-import { classNames } from "@hkamran/utility-web";
-
 import { links } from "@/data/navigation";
 
 import NavLink from "./navigation/NavLink";
 import SocialLinks from "./navigation/SocialLinks";
 
 const Footer = () => (
-    <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-3 sm:gap-6">
+    <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-6">
         <div className="space-y-4 md:text-left">
-            <p>
-                Copyright &copy; {new Date().getFullYear()} H. Kamran. All
-                rights reserved.
-            </p>
+            <div>
+                <p>
+                    Copyright &copy; {new Date().getFullYear()} H. Kamran. All
+                    rights reserved.
+                </p>
+                <p>
+                    This site is designed and made exclusively by humans only.
+                    No &quot;AI&quot; was used in the making of any of the
+                    content on this site.
+                </p>
+            </div>
             <div className="flex flex-row justify-center space-x-5 md:justify-start">
-                <SocialLinks classNames="h-4 w-4 fill-gray-500 transition-colors duration-200 ease-in-out hover:fill-gray-300" location="footer" />
+                <SocialLinks
+                    classNames="h-4 w-4 fill-gray-500 transition-colors duration-200 ease-in-out hover:fill-gray-300"
+                    location="footer"
+                />
             </div>
         </div>
-        <div className="col-span-2 space-y-4 md:text-right">
+        <div className="space-y-4 md:text-right">
             <p className="space-x-3">
                 {links.map(({ href, name }, index) => (
                     <NavLink
