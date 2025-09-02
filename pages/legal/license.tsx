5bc2aec1fe1611b822df2841182ae2e52431c1a2
H. Kamran <hkamran@hkamran.com>
2025-09-02 15:57:20 -0700

    Add "AI" license

--- pages/legal/license.tsx
+++ pages/legal/license.tsx
@@ -26,18 +26,21 @@ const License: NextPage = () => (
 
             <PageHeader
                 name="License"
-                description="Last Updated: January 31, 2023"
+                description="Last Updated: September 2, 2025"
             />
 
             <div className="prose prose-invert mt-6 max-w-none prose-a:text-pink-700">
                 <p>
-                    Unless otherwise noted, the copyright of all content on this site (and its redirects) belongs to H. Kamran. This includes, but is not limited to, text and images, visual design, source code, articles, notes,
-                    programs, showcase items, photos, and any other
-                    content on this site.
+                    Unless otherwise noted, the copyright of all content on this
+                    site (and its redirects) belongs to H. Kamran. This
+                    includes, but is not limited to, text and images, visual
+                    design, source code, articles, notes, programs, showcase
+                    items, photos, and any other content on this site.
                 </p>
 
                 <p>Copyright &copy; 2025 H. Kamran. All rights reserved.</p>
 
+                <hr />
 
                 <ul>
                     <li>
@@ -109,8 +112,79 @@ const License: NextPage = () => (
                     <NavLink href="https://unsplash.com/license">
                         Unsplash License
                     </NavLink>
-                    . Please provide attribution, even though it is not required
-                    by the Unsplash License.
+                    . Attribution is strongly recommended.
+                </p>
+
+                <hr />
+
+                <h3 id="ai-license">&quot;AI&quot; License</h3>
+                <p>
+                    Content may be licensed for &quot;AI&quot;-related use. A
+                    license is required on a per-project basis for purposes
+                    including, but not limited to, &quot;artificial
+                    intelligence&quot;, vibe coding, data mining, data indexing,
+                    data hoarding, deep learning, machine learning, large
+                    language models, small language models, neural networks, and
+                    development and training of any &quot;AI&quot;-related
+                    software.
+                </p>
+
+                <h4 id="ai-license-fees">License Fees</h4>
+                <p>There are two license fee options available.</p>
+                <ul>
+                    <li>
+                        Single license fee of $100,000.00 (one hundred thousand
+                        USD) for all content
+                    </li>
+                    <li>
+                        $1.00 (one USD) per Unicode character of content (UTF-8)
+                    </li>
+                </ul>
+
+                <p>United States Dollars (USD) only. No cryptocurrency.</p>
+
+                <p className="text-xs">
+                    For the purposes of this section only, &quot;content&quot;
+                    refers to any content published on or before the date of
+                    purchase.
+                </p>
+
+                <h4 id="ai-license-terms-and-conditions">
+                    Terms and Conditions
+                </h4>
+                <p>
+                    You accept and agree to these license terms by downloading,
+                    cloning, extracting, mirroring, ripping, photographing,
+                    scraping, syndicating, or by otherwise copying content from
+                    this website for &quot;AI&quot; related use as defined
+                    above.
+                </p>
+
+                <p>
+                    Content is provided &quot;as is&quot; without warranty. You
+                    are responsible for obtaining any content you want to
+                    license. I will not provide assistance or support. I do not
+                    guarantee the accuracy or quality of content. I do not
+                    guarantee that any content will remain accessible. This
+                    license does not include any new or edited content after the
+                    date of purchase. I will not be liable for any damages
+                    arising from use of the content. All license fees are
+                    non-refundable. I reserve the right to refuse or revoke this
+                    license at any time.
+                </p>
+
+                <p>
+                    You must email hkamran@hkamran.com within 30 days of
+                    accepting these terms. Please include your project and
+                    business details, content usage, and the license fee you
+                    want to pay. I will reply to your email with a PDF invoice
+                    attached. The invoice must be paid within 30 days.
+                </p>
+
+                <p>
+                    By accessing this website and obtaining content for
+                    &quot;AI&quot;-related purposes, you automatically opt-in
+                    and agree to these terms.
                 </p>
             </div>
         </div>
