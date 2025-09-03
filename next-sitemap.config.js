efcc36b15e1418cbd930e74f6c6e8bfb6af0d79c
H. Kamran <hkamran@hkamran.com>
2025-09-02 19:43:53 -0700

    Remove robots.txt exclusion policies
    For now, all LLM/"AI"-adjacent crawlers are being exempted from the
    robots.txt file because as [Drew DeVault
    said](https://drewdevault.com/2025/03/17/2025-03-17-Stop-externalizing-your-costs-on-me.html):
    "If you think these crawlers respect robots.txt then you are several
    assumptions of good faith removed from reality." If this system does
    not work long-term, then I'll switch to the [ai.robots.txt project](https://github.com/ai-robots-txt/ai.robots.txt/).

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,70 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 
-// Source: https://www.foundationwebdev.com/2023/11/which-web-crawlers-are-associated-with-ai-crawlers
-const llmUserAgents = [
-    // Anthrophic
-    "anthropic-ai",
-    "Claude-Web",
-
-    // Apple
-    "Applebot-Extended",
-
-    // ByteDance
-    "Bytespider",
-
-    // Common Crawl
-    "CCBot",
-
-    // OpenAI
-    "ChatGPT-User",
-    "GPTBot",
-
-    // Cohere AI
-    "cohere-ai",
-
-    // Diffbot
-    "Diffbot",
-
-    // Meta
-    "FacebookBot",
-
-    // Google
-    "GoogleOther",
-    "Google-Extended",
-
-    // The Hive
-    "ImagesiftBot",
-
-    // Webz.io
-    "OmigiliBot",
-    "Omigili",
-];
-
-// Source: https://darkvisitors.com/agents
-const otherUserAgents = [
-    // Adbeat
-    "adbeat_bot",
-
-    // Google
-    "Adsbot-Google",
-    "Adsbot-Google-Mobile",
-
-    // aiHit
-    "aiHitBot",
-
-    // Anders Pink
-    "AndersPinkBot",
-
-    // Awario
-    "AwarioBot",
-    "AwarioSmartBot",
-
-    // Bitsight
-    "BitSightBot",
-];
-
-const fullyBlockedUserAgents = [...llmUserAgents, ...otherUserAgents];
-
 module.exports = {
     siteUrl: "https://hkamran.com",
     changefreq: "daily",
@@ -79,20 +14,6 @@ module.exports = {
                 allow: "/",
                 disallow: ["/_next/", "/api/", "/*.png"],
             },
-
-            ...fullyBlockedUserAgents.map((userAgent) => ({
-                userAgent,
-                disallow: "/",
-            })),
-
-            //{
-            //    userAgent: "GPTBot",
-            //    disallow: "/",
-            //},
-            //{
-            //    userAgent: "Applebot-Extended",
-            //    disallow: "/",
-            //},
         ],
     },
 };
