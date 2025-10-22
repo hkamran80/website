94cfa68617f4656722824a00f3d9f997a6958e66
H. Kamran <hkamran@hkamran.com>
2025-10-21 20:13:17 -0700

    Add new front page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://hkamran.com",
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [svelte()],
 });
---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,47 @@
+<script>
+import {Sun,Moon}from "@lucide/svelte"
+
+  const rootEl = typeof document !== 'undefined' ? document.documentElement : null;
+  const themes = ['light', 'dark'];
+  let theme = ''
+
+  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  } else if (typeof window !== 'undefined' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    theme = 'dark';
+  }
+
+  function handleChange(event) {
+    theme = event.target.value;
+    localStorage.setItem('theme', theme);
+  }
+
+  $: if (rootEl && theme === 'light') {
+    rootEl.classList.remove('theme-dark');
+  } else if (rootEl && theme === 'dark') {
+    rootEl.classList.add('theme-dark');
+  }
+
+  const icons = [
+    Sun,
+    Moon
+  ];
+</script>
+
+<div class="theme-toggle">
+  {#each themes as t, i}
+          {@const Icon = icons[i]}
+    <label class={theme === t ? 'checked' : ''}>
+            <Icon size="20"/>
+      <input
+        type="radio"
+        name="theme-toggle"
+        checked={theme === t}
+        value={t}
+        title={`Use ${t} theme`}
+        aria-label={`Use ${t} theme`}
+        on:change={handleChange}
+      />
+    </label>
+  {/each}
+</div>
