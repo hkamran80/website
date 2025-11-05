3d8608067078b14f39517c6017ab61f93fe2f18c
H. Kamran <hkamran@hkamran.com>
2025-11-05 00:38:08 -0800

    Move theme toggle CSS to component

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -29,19 +29,67 @@ import {Sun,Moon}from "@lucide/svelte"
 </script>
 
 <div class="theme-toggle">
-  {#each themes as t, i}
-          {@const Icon = icons[i]}
-    <label class={theme === t ? 'checked' : ''}>
+    {#each themes as t, i}
+        {@const Icon = icons[i]}
+        <label for={`theme-toggle-${t}`} class={theme === t ? 'checked' : ''}>
+            <span class="sr-only">Use {t} theme</span>
             <Icon size="20"/>
-      <input
-        type="radio"
-        name="theme-toggle"
-        checked={theme === t}
-        value={t}
-        title={`Use ${t} theme`}
-        aria-label={`Use ${t} theme`}
-        on:change={handleChange}
-      />
-    </label>
-  {/each}
+
+            <input
+                type="radio"
+                name={`theme-toggle-${t}`}
+                checked={theme === t}
+                value={t}
+                title={`Use ${t} theme`}
+                aria-label={`Use ${t} theme`}
+                on:change={handleChange}
+              />
+        </label>
+    {/each}
 </div>
+
+<style>
+.theme-toggle {
+    display: inline-flex;
+    align-items: center;
+    height: 100%;
+    padding: 0.33em 0.67em;
+    padding-top: 8px;
+    margin-left: 10px;
+    gap: 0.6em;
+    border-radius: 99em;
+    background-color: var(--theme-code-inline-bg);
+}
+
+.theme-toggle > label:focus-within {
+    outline: 2px solid transparent;
+    box-shadow:
+        0 0 0 0.08em var(--theme-accent),
+        0 0 0 0.12em white;
+}
+
+.theme-toggle > label {
+    color: var(--theme-code-inline-text);
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    opacity: 0.5;
+    cursor: pointer;
+}
+
+.theme-toggle .checked {
+    color: var(--theme-accent);
+    opacity: 1;
+}
+
+input[type="radio"] {
+    position: absolute;
+    opacity: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+}
+</style>
