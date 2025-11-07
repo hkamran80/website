a05478c5edd1fb3aacba93e42b2b0b0afd84e6aa
H. Kamran <hkamran@hkamran.com>
2025-11-06 16:01:04 -0800

    Add links to the footer, use shared constants object

---
+++ src/navigation.ts
@@ -0,0 +1,7 @@
+export const links: { href: string; name: string; aliases?: string[] } = [
+    { href: "/projects", name: "Projects", aliases: [] },
+    { href: "/articles", name: "Articles", aliases: ["/article", "/tag"] },
+    { href: "/notes", name: "Notes", aliases: ["/note", "/tag"] },
+    { href: "/programs", name: "Programs", aliases: ["/program"] },
+    { href: "https://unsplash.com/@hkamran", name: "Photos", aliases: [] },
+];
