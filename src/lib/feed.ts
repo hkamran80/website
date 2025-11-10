2718f43dd7dfdfcb27639ef3df281e37f55a1a48
H. Kamran <hkamran@hkamran.com>
2025-11-09 17:44:20 -0800

    Add feeds

---
+++ src/lib/feed.ts
@@ -0,0 +1,74 @@
+import { type Author, Feed, type Item } from "feed";
+import type { CollectionEntry } from "astro:content";
+
+const feedImageLengths: Record<string, number> = {};
+
+export const generateFeed = async (site: string, posts: CollectionEntry<"posts">) => {
+    const author: Author = {
+        name: "H. Kamran",
+        email: "hkamran@hkamran.com",
+        link: site,
+    };
+
+    const feed = new Feed({
+        title: "H. Kamran",
+        description: "The feed of both my articles and my notes",
+        id: site,
+        link: site,
+        language: "en",
+        image: `${site}profile.png`,
+        favicon: `${site}favicon.png`,
+        copyright: `© ${new Date().getFullYear()} H. Kamran. All rights reserved.`,
+        updated: posts[0].data.updated || posts[0].data.published,
+        feedLinks: {
+            rss: `${site}feed.rss`,
+            atom: `${site}feed.atom`,
+            json: `${site}feed.json`,
+        },
+        author,
+    });
+
+    for (const post of posts) {
+        let entry: Item = {
+            id: post.id,
+            title: post.data.title,
+            link: `${site}${post.data.type}s/${post.id}`,
+            description: post.data.description,
+            // TODO: Use full-text
+            content: post.data.description,
+            date: post.data.published,
+            author: [author],
+        };
+
+        if (post.data.type === "article") {
+            let length = 0;
+
+            if (post.id in feedImageLengths) length = feedImageLengths[post.id];
+            else {
+                const response = await fetch(
+                    `https://assets.hkamran.com/graphics/article/${post.id}`,
+                    { method: "HEAD" },
+                );
+                const contentLength = response.headers.get("content-length");
+                if (contentLength) {
+                    length = Number(contentLength);
+                    feedImageLengths[post.id] = length;
+                } else
+                    console.error(
+                        "Failed to load Content-Length",
+                        response.headers,
+                    );
+            }
+
+            entry.image = {
+                url: `https://assets.hkamran.com/graphics/article/${post.id}`,
+                type: "image/png",
+                length,
+            };
+        }
+
+        feed.addItem(entry);
+    };
+
+    return feed;
+};
---
+++ src/pages/feed.json.ts
@@ -0,0 +1,25 @@
+import { getCollection } from "astro:content";
+import type { APIRoute } from "astro";
+import { sortByDate } from "../lib/sort.ts";
+import { generateFeed } from "../lib/feed.ts";
+
+export const GET: APIRoute = async ({ site: siteUrl }) => {
+    if (!siteUrl) return new Response(undefined, { status: 500 });
+
+    const posts = (
+        await getCollection("posts", ({ data }) => data.published !== "")
+    ).sort((a, b) =>
+        sortByDate(new Date(a.data.published), new Date(b.data.published)),
+    );
+
+    const site = siteUrl.toString();
+    const feed = await generateFeed(site, posts);
+
+    return new Response(feed.json1(), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+};
+
+
