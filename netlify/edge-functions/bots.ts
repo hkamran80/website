b7cc4d9e5c9c9fb89d96a7d33f76816236b155f6
H. Kamran <hkamran@hkamran.com>
2025-12-16 19:23:41 -0800

    Exempt `robots.txt`, undo UA space replacement

--- netlify/edge-functions/bots.ts
+++ netlify/edge-functions/bots.ts
@@ -2,6 +2,7 @@ import type { Config } from "@netlify/edge-functions";
 
 export const config: Config = {
     path: "/*",
+    excludedPath: "/robots.txt",
 };
 
 const robotsTxt = await fetch(
@@ -15,20 +16,19 @@ const bots = txt
         (line) =>
             line.startsWith("User-agent:") && line !== "User-agent: Applebot",
     )
-    .map((line) =>
-        line.split(": ")[1].trim().replaceAll(" ", "\\ ").toLowerCase(),
-    );
+    .map((line) => line.split(": ")[1].trim().toLowerCase());
 
 export default async (request: Request) => {
     const userAgent =
         request.headers.get("user-agent")?.toLowerCase() ?? undefined;
-    console.log(`Request from ${userAgent}`)
+    console.log(`Request from ${userAgent}`);
 
     if (!userAgent) return;
 
     // Check against our list of known "AI" bots
-    const isBot = bots.some((bot) => userAgent.includes(bot));
-    console.log(`  ${isBot ? "Blocking" : "Allowing"} request.`)
+    const isBot =
+        bots.some((bot) => userAgent.includes(bot)) || userAgent === "google";
+    console.log(`  ${isBot ? "Blocking" : "Allowing"} request.`);
 
     // If the requester is an AI bot, redirect to a 10 GB file 😈
     if (isBot)
