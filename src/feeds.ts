6642773a2e121375da6605654ab40a3fa2b11114
H. Kamran <hkamran@hkamran.com>
2025-12-23 20:49:41 -0800

    Add feeds page

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,1 +1,12 @@
+export type Feed = { name: string; description: string }
 export type FeedTypes = "all" | "articles" | "notes"
+
+export const feeds: Record<
+    FeedTypes,
+    Feed
+> = {
+    all: { name: "All posts", description: "All my articles and notes" },
+    articles: { name: "Articles", description: "All my articles" },
+    notes: { name: "Notes", description: "All my notes" },
+};
+
