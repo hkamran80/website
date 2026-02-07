118c20fbdfd88bb0d0682ef0a8c4472bf3657871
H. Kamran <hkamran@hkamran.com>
2026-02-07 00:23:09 -0500

    feeds: Add TODOs

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,6 +1,8 @@
 export type Feed = { name: string; description: string };
 export type FeedTypes = "all" | "posts" | "bill-analyses";
 
+// TODO: Create photos feed
+// TODO: Create now feed
 export const feeds: Record<FeedTypes, Feed> = {
     all: { name: "All", description: "All posts and bill analyses" },
     posts: { name: "Posts", description: "All posts" },
