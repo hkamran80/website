d7dfd8112740cff9b136ca0a189ed8a2fbf3fbc3
H. Kamran <hkamran@hkamran.com>
2026-03-16 12:27:45 -0700

    feeds: Only add featured feeds to `head`

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,11 +1,15 @@
-export type Feed = { name: string; description: string };
+export type Feed = { name: string; description: string; featured?: boolean };
 export type FeedTypes = "all" | "posts" | "bill-analyses";
 
 // TODO: Create photos feed
 // TODO: Create now feed
 export const feeds: Record<FeedTypes, Feed> = {
-    all: { name: "All", description: "All posts and bill analyses" },
-    posts: { name: "Posts", description: "All posts" },
+    all: {
+        name: "All",
+        description: "All posts and bill analyses",
+        featured: true,
+    },
+    posts: { name: "Posts", description: "All posts", featured: true },
     "bill-analyses": {
         name: "Bill Analyses",
         description: "All bill analyses",
